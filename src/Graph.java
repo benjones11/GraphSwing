aeb531bf2df36288593773da9fc35bd9fec6a1b1
Benjamin Jones <benjaminjones19@yahoo.com>
2025-09-15 19:11:44 -0700

    Added a "Redraw" button and aligned the bars to the bottom of the graph

--- src/Graph.java
+++ src/Graph.java
@@ -1,6 +1,5 @@
 import javax.swing.*;
 import java.awt.*;
-
 import java.util.Random; // for random bar height and color generation
 
 public class Graph extends JPanel {
@@ -17,6 +16,7 @@ public class Graph extends JPanel {
     @Override
     protected void paintComponent(Graphics graphics) {
         // Extend using the Graphics2D library so we can add objects to the canvas
+        super.paintComponent(graphics); //clear before re-drawing*
         Graphics2D g2 = (Graphics2D) graphics;
 
         // Calculate x and y coordinates of the intial bar based off of the frame x and y
@@ -27,11 +27,15 @@ public class Graph extends JPanel {
         for (int gridX = 30; gridX <= 300; gridX += 30)
             for (int gridY = 30; gridY <= 300; gridY += 30) 
                 g2.drawRect(gridX, gridY, 30, 30);
-        
-        Random random = new Random(); // for random heights and random colors
+
+        //set y to the bottom of the grid
+        final int GRIDBottom = 30 + 10 * 30;
+
+        y = GRIDBottom; // for random heights and random colors
+        Random random = new Random();
 
         // Draw 12 bars
-        for (int i = 0; i < 12; i++) {
+        for (int i = 0; i < 12; i++) { //changed 12 to num_bars*
             // Get random height 0-200
             BAR_HEIGHT = random.nextInt(200);
 
@@ -52,4 +56,33 @@ public class Graph extends JPanel {
         }
 
     }
+    //reprinting the bars as soon as the button is clicked
+    public void Randomize() {
+        Graphics2D g2 = (Graphics2D) this.getGraphics();
+        int x = (int) (this.getWidth() * 0.1);
+        int y = (int) (this.getHeight() * 0.91);
+
+        Random random = new Random();
+        for (int i = 0; i < 12; i++) { //changed 12 to num_bars*
+            // Get random height 0-200
+            BAR_HEIGHT = random.nextInt(200);
+
+            // Random colors
+            float r = random.nextFloat();
+            float g = random.nextFloat();
+            float b = random.nextFloat();
+
+            // Draw bars
+            int rectangleWidth = (int) (BAR_WIDTH);
+            int rectangleHeight = (int) (BAR_HEIGHT);
+            Rectangle rectangle = new Rectangle(x, y - BAR_HEIGHT, rectangleWidth, rectangleHeight);
+            g2.setColor(new Color(r, g, b));
+            g2.fill(rectangle);
+
+            // Bars are 20 pixels apart
+            x = x + 20;
+            repaint(); //Calling re-print after button is clicked
+        }
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
-import javax.swing.JFrame;
-
+import javax.swing.*;
+import java.awt.*;//in-order to use Flow Layout
 public class Main {
     public static void main(String[] args) {
         // Create a new JFrame
@@ -15,5 +15,20 @@ public class Main {
         Graph barChart = new Graph(10);
         frame.add(barChart);  
         frame.setVisible(true);
+
+        //creating a "Redraw" button
+        JButton Redraw = new JButton("Redraw");
+        //each time the button is clicked, a new bar graph is created
+        Redraw.addActionListener(e->barChart.repaint());
+
+        JPanel south = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        south.setBorder(BorderFactory.createEmptyBorder(8, 0, 8, 0));
+        south.add(Redraw);
+        frame.add(south, BorderLayout.SOUTH);
+
+        frame.setSize(400, 400);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+
     }
 }
